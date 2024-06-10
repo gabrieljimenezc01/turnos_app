d7899cd551b668b82ba047d1b8db0e1b0c9f84b9
gabrieljimenezc01 <jesusgabrieljimenezceron@gmail.com>
2024-06-10 11:27:08 -0500

    actualizacion base de datos

--- db.sql
+++ db.sql
@@ -15,3 +15,14 @@ CREATE TABLE turnos (
 );
 
 INSERT INTO cajas (nombre) VALUES ('Caja 1'), ('Caja 2'), ('Caja 3');
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE turnos ADD COLUMN cliente_id INT;
+ALTER TABLE turnos ADD CONSTRAINT fk_cliente FOREIGN KEY (cliente_id) REFERENCES clientes(id);
+
