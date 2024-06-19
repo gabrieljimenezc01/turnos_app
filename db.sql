1863729d8b4eb02659b8422e3d3d0e201fd555d0
gabrieljimenezc01 <jesusgabrieljimenezceron@gmail.com>
2024-06-18 23:57:55 -0500

    actualizacion db

--- db.sql
+++ db.sql
@@ -16,19 +16,6 @@ CREATE TABLE turnos (
 
 INSERT INTO cajas (nombre) VALUES ('Caja 1'), ('Caja 2'), ('Caja 3');
 
-CREATE TABLE clientes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    telefono VARCHAR(20) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
-
-ALTER TABLE turnos ADD COLUMN cliente_id INT;
-ALTER TABLE turnos ADD CONSTRAINT fk_cliente FOREIGN KEY (cliente_id) REFERENCES clientes(id);
-
--- Eliminar la tabla de clientes si existe
-DROP TABLE IF EXISTS clientes;
-
 -- Crear la tabla de clientes con la cédula como clave primaria
 CREATE TABLE clientes (
     cedula VARCHAR(20) PRIMARY KEY,
@@ -37,8 +24,7 @@ CREATE TABLE clientes (
 );
 
 -- Modificar la tabla de turnos para referenciar la tabla de clientes por cédula
-ALTER TABLE turnos DROP FOREIGN KEY fk_cliente;
-ALTER TABLE turnos DROP COLUMN cliente_id;
+
 ALTER TABLE turnos ADD COLUMN cliente_cedula VARCHAR(20);
 ALTER TABLE turnos ADD CONSTRAINT fk_cliente FOREIGN KEY (cliente_cedula) REFERENCES clientes(cedula);
 
@@ -47,16 +33,16 @@ CREATE TABLE servicios (
     nombre VARCHAR(255) NOT NULL
 );
 
-INSERT INTO servicios (nombre) VALUES ('Deposito'), ('Retiro'), ('Servicios Publicos'), ('Asesoria'), ('Otros Servicios');
+INSERT INTO servicios (nombre) VALUES ('cajero'), ('asesor'),  ('otros');
 
 CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    rol ENUM('cajero', 'asesor', 'otros') NOT NULL,
     servicio_id INT,
     FOREIGN KEY (servicio_id) REFERENCES servicios(id)
 );
 
-ALTER TABLE usuarios DROP COLUMN rol;
+ALTER TABLE turnos ADD CONSTRAINT fk_servicio FOREIGN KEY (servicio_id) REFERENCES servicios(id);
+
 
